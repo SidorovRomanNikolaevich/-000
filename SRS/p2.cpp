c004066885966c69213d3f705ef45cb6fc4e399b
SidorovRomanNikolaevich <154632627+SidorovRomanNikolaevich@users.noreply.github.com>
2023-12-23 00:08:01 +0300

    Update p2.cpp

--- SRS/p2.cpp
+++ SRS/p2.cpp
@@ -1,10 +1,12 @@
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main() {
     FILE *logfile;
-    logfile = fopen("log.txt", "w"); // открыть файл log.txt для записи
+    char path[] = "files/log.txt"; // путь к файлу log.txt
+
+    logfile = fopen(path, "w"); // открыть файл log.txt для записи
 
     if (logfile == NULL) {
         printf("Ошибка открытия файла");
@@ -15,11 +17,17 @@ int main() {
         system("clear"); // очистить экран консоли
 
         // Получить информацию о текущем использовании ресурсов
-        FILE *result = popen("ps aux | grep <p1.cpp>", "r");
+        FILE *result = popen("ps aux | grep <название_вашей_программы>", "r");
         char buffer[1024];
         while (fgets(buffer, 1024, result) != NULL) {
-            fprintf(logfile, "%s", buffer); // записать результаты в файл
             printf("%s", buffer); // вывести результаты на экран
+        }
+        pclose(result);
+
+        // Записать результаты в файл
+        result = popen("ps aux | grep <название_вашей_программы>", "r");
+        while (fgets(buffer, 1024, result) != NULL) {
+            fprintf(logfile, "%s", buffer); // записать результаты в файл
         }
         pclose(result);
 
