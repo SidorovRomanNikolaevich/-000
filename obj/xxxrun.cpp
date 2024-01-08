67af7201dfbeb21f15e0d2720d364f65f4d785c1
SidorovRomanNikolaevich <154632627+SidorovRomanNikolaevich@users.noreply.github.com>
2024-01-08 23:32:56 +0300

    Update xxxrun.cpp

--- obj/xxxrun.cpp
+++ obj/xxxrun.cpp
@@ -1,10 +1,6 @@
 #include <cstdlib>
 int main() {
     // Запуск программы p1.cpp
-    system("g++ ../src/p1.cpp -o p1 && ./p1");
-
-    // Запуск программы p2.cpp
-    system("g++ ../src/p2.cpp -o p2 && ./p2");
-
+    system("g++ ../SRC/p1.cpp -o p1 && ./p1");
     return 0;
 }
